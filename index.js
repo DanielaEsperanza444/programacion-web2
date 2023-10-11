21b374a4de1c93d47b588582aa12cfafd55b0cc9
DanielaEsperanza444 <147216740+DanielaEsperanza444@users.noreply.github.com>
2023-10-11 01:45:42 +0000

    prueba

---
+++ index.js
@@ -0,0 +1,24 @@
+function suma(a, b) {
+    return a + b
+}
+
+const resta = function(a, b) {
+    return a - b
+}
+
+const producto = (a, b) => a * b
+
+const nombreCompleto = (nombre) => `Hola, cómo estas ${nombre}`
+
+const resultado_suma = suma(2, 8)
+const resultado_resta = resta(1, 5)
+const resultado_producto = producto(3, 6)
+
+console.log(resultado_suma)
+console.log(resultado_resta)
+console.log(resultado_producto)
+console.log(nombreCompleto('Carlos'));
+
+
+const saludo = (nombre) => `Hola, cómo estas ${nombre}`
+console.log(saludo('Daniela!!'));
